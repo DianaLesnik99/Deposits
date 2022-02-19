ba6a1a279d88e425415959a4ff0e75aeefb7ddec
DianaLesnik <dianalesnik99@gmail.com>
2022-02-19 21:47:58 +0900

    Добавлена страница с историей развития, добавлены оглавление, кнопки навигации вверх и вниз

---
+++ static/js/scripts.js
@@ -0,0 +1,23 @@
+jQuery(function () {
+    $(".GoTop").hide();
+    if ($(window).scrollTop() >= 250) $(".GoTop").fadeIn("slow")
+    $(window).scroll(function () {
+        if ($(window).scrollTop() <= 250) $(".GoTop").fadeOut("slow")
+        else $(".GoTop").fadeIn("slow")
+    });
+
+    $(".GoBottom").hide();
+    if ($(window).scrollTop() <= $(document).height()-"999") $(".GoBottom").fadeIn("slow")
+    $(window).scroll(function () {
+        if ($(window).scrollTop() >= $(document).height()-"999") $(".GoBottom").fadeOut("slow")
+        else $(".GoBottom").fadeIn("slow")
+    });
+
+    $(".GoTop").click(function () {
+        $("html, body").animate({scrollTop: 0}, "slow")
+    })
+
+    $(".GoBottom").click(function () {
+        $("html, body").animate({scrollTop: $(document).height()}, "slow")
+    })
+});
---
+++ static/js/test.js
@@ -0,0 +1,15 @@
+alert ('Подключён, начинаю работу!')
+$(document).ready(function() {
+    console.log('ready!');
+});
+function onReady(){
+    $("#forward").on("click", function(){
+        $("h1").text("Йеее! Мы поменяли текст заголовка!")
+    })
+
+    $("#backward").on("click", function(){
+        $("h1").text("Это Главная страница сайта")
+    })
+}
+
+$(document).ready(onReady);
